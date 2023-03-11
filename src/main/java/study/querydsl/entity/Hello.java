aa76f704f98d6798e15bec578d97786b9a7ec2c8
ing9990 <gimgau0218@naver.com>
2023-03-12 02:41:33 +0900

    Initial commit

---
+++ src/main/java/study/querydsl/entity/Hello.java
@@ -0,0 +1,25 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
